f9fa2a4e1c346ffe19335d1b0ed74e42b2fba0eb
Júlia Muniz <julia.mco0512@gmail.com>
2025-02-22 12:41:46 +0000

    Atividade finalizada

--- ex03-condicionais/Atividade01.java
+++ ex03-condicionais/Atividade01.java
@@ -4,20 +4,29 @@ public class Atividade01 {
     public static Scanner input = new Scanner(System.in);
 
     public static void main(String[] args) {
-        int peso, altura;
-        System.out.print("Digite sua altura: ");
-        altura = input.nextInt();
-        System.out.println("Digite seu peso:");
-        peso = input.nextInt();
+        double peso, altura;
+        System.out.print("Digite sua altura em metros: ");
+        altura = input.nextDouble();
+        System.out.print("Digite seu peso em kg:");
+        peso = input.nextDouble();
 
-        int imc = peso/ (altura * altura);
+        double imc = peso/ (altura * altura);
 
-        if(imc > 18.5) {
+        if(imc < 18.5) {
             System.out.println("Você está abaixo do peso normal!");
         } else if (imc < 25) {
             System.out.println("Você está no peso normal!");
-        } else if (imc )
-
+        } else if (imc < 30) {
+            System.out.println("Você está com excesso de peso!");
+        } else if (imc < 35) {
+            System.out.println("Você está com obesidade classe 1!");
+        } else if (imc < 40) {
+            System.out.println("Você está com obesidade classe 2!");
+        } else {
+            System.out.println("Você está com obesidade clase 3!");
+        }
+        
+        System.out.println("Seu IMC é: " + imc);
     }
     
 }
