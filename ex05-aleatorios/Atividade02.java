1df7211956cc6d6f08e4a7e97b1d77331c891530
Júlia Muniz <julia.mco0512@gmail.com>
2025-03-14 04:46:44 +0000

    Atividade terminada

--- ex05-aleatorios/Atividade02.java
+++ ex05-aleatorios/Atividade02.java
@@ -13,16 +13,16 @@ public class Atividade02 {
         int jogador1 = geradorAleat.nextInt(6) + 1;
         int jogador2 = geradorAleat.nextInt(6) + 1;
     
-        System.out.println("Rodada " + rodada + ": O jogador 1 tirou " + jogador1 + ", O jogador 2 tirou " + jogador2);
+        System.out.println("Rodada " + rodada + ": O jogador 1: " + jogador1 + " - O jogador 2: " + jogador2);
 
         if (jogador1 > jogador2) {
             pontos1++;
-            System.out.println("O jogador 1 venceu esta rodada!\n");
+            System.out.println("Jogador 1 ganhou!\n");
         } else if (jogador2 > jogador1) {
             pontos2++;
-            System.out.println("O jogador 2 venceu esta rodada!\n");
+            System.out.println("Jogador 2 ganhou!\n");
         } else {
-            System.out.println("Ocorreu um empate entre os jogadores nesta rodada!\n");
+            System.out.println("Empatou!\n");
         }
     
     }
@@ -30,11 +30,11 @@ public class Atividade02 {
     // Mostrar a pontuação final
     System.out.println("=== RESULTADO FINAL ===");
     if (pontos1 > pontos2) {
-        System.out.println("O jogador 1 ganhou por " + pontos1 + " x " + pontos2);
+        System.out.println("Jogador 1 ganhou por " + pontos1 + " x " + pontos2 + "!");
     } else if (pontos2 > pontos1) {
-        System.out.println("O jogador 2 ganhou por " + pontos2 + " x " + pontos1);
+        System.out.println("Jogador 2 ganhou por " + pontos2 + " x " + pontos1 + "!");
     } else {
-        System.out.println("Houve um empate no jogo " + pontos1 + " x " + pontos2);
+        System.out.println("Empate no jogo " + pontos1 + " x " + pontos2 + "!");
     }
 
 }
