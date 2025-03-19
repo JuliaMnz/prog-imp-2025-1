9260ec9cce742fe06456d278e7cdef29d884df24
Júlia Muniz <julia.mco0512@gmail.com>
2025-03-19 16:29:31 -0300

    questao01.java

--- atv03/questao01.java
+++ atv03/questao01.java
@@ -21,13 +21,17 @@ public class questao01 {
         if (diasAtraso >= 45) {
             System.out.println("O cálculo da conta não pode ser realizado. A conta está atrasada há 45 dias ou mais.");
         } else {
+            
             // Cálculo da multa
             double multa = valorConta * (valorMulta / 100);
+            
             //Cálculo dos juros
             double jurosDiarios = (valorJuros / 100) / 30;
             double juros = valorConta * jurosDiarios * diasAtraso;
+            
             //Cálculo do valor final da conta
             double valorFinal = valorConta + multa + juros;
+            
             // Resultado final
             System.out.println("Valor inicial da conta: R$ " + valorConta);
             System.out.println("Multa aplicada: R$ " + multa);
